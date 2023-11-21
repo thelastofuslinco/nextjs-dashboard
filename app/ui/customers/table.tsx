c6462c2377b860e71ff65c11ae9103957c60d4b9
lincoln Duarte <lincolnskyrim_99@hotmail.com>
2023-11-21 10:30:09 -0300

    style: prettier

--- app/ui/customers/table.tsx
+++ app/ui/customers/table.tsx
@@ -1,13 +1,13 @@
 import Image from 'next/image';
 import { lusitana } from '@/app/ui/fonts';
 import Search from '@/app/ui/search';
-import { CustomersTable, FormattedCustomersTable } from '@/app/lib/definitions';
+import { FormattedCustomersTable } from '@/app/lib/definitions';
 
 export default async function CustomersTable({
   customers,
-}: {
+}: Readonly<{
   customers: FormattedCustomersTable[];
-}) {
+}>) {
   return (
     <div className="w-full">
       <h1 className={`${lusitana.className} mb-8 text-xl md:text-2xl`}>
